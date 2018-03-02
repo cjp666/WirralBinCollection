04c411fe4ddf4b21fcc6e656bca9f0623ef28c7d
cjp666 <chris.j.podmore@gmail.com>
2018-03-02 19:54:02 +0000

    update parsing of html to get date

--- functions/src/retrieveBin.ts
+++ functions/src/retrieveBin.ts
@@ -1,4 +1,6 @@
 import * as request from 'request-promise-native';
+import * as moment from 'moment';
+const DOMParser = require('xmldom').DOMParser;
 
 export class RetrieveBin {
     async retrieve(street: string, town: string): Promise<string> {
@@ -22,8 +24,38 @@ export class RetrieveBin {
         };
 
         const result = await request.get(options);
-        console.log(result);
 
-        return result.toString();
+        return this.retrieveDataFromHtml(result);
+    }
+
+    retrieveDataFromHtml(html: string): string {
+        const doc = new DOMParser().parseFromString(html, 'application/html');
+        const legends = doc.getElementsByTagName('legend');
+        if (!!legends) {
+            for (const legend in legends) {
+                const l = legends[legend];
+                if (!!l.firstChild && !!l.firstChild.firstChild) {
+                    const data = l.firstChild.firstChild.data;
+                    const date = this.createDateFromData(data);
+                    return date;
+                }
+            }
+        }
+
+        return '';
+    }
+
+    createDateFromData(data: string): string {
+        if (!!data === false) {
+            return null;
+        }
+        const dataParts = data.split(',');
+        const dateParts = dataParts[1].trim().split('/');
+        const day = Number(dateParts[0]);
+        const month = Number(dateParts[1]) - 1;
+        const year = Number(dateParts[2]);
+        const date = new Date(year, month, day, 0, 0, 0, 0);
+        const result = moment(date).format('dddd, Do MMMM');
+        return result;
     }
 }
--- functions/test/retrieveBin.spec.ts
+++ functions/test/retrieveBin.spec.ts
@@ -16,6 +16,32 @@ describe('Retrieve bin', () => {
     it("should get something for 'grey'", async () => {
         const rb = new RetrieveBin();
         const result = await rb.retrieveBin('Riviera Drive', 'Rock Ferry', 'grey');
+        console.log(result);
         result.should.not.be.equal('');
+    });
+});
+
+describe('retrieveDataFromHtml', () => {
+    let html;
+    beforeEach(() => {
+        html =
+            '<body>' +
+            '	<label style="color: white; background-color:#999; font-weight: Bold; width:90%;">' +
+            '    	&nbsp;The Dates Below Are For<br/>&nbsp;Paper and packaging (grey bin)' +
+            '    	<br/>&nbsp;The next collection is:<br />' +
+            '	</label>' +
+            '' +
+            '	<fieldset style="padding-left:3px; border:none;">' +
+            '' +
+            '' +
+            '		<legend><span style="color: black; font-weight: Bold;">Wed, 14/03/2018</span></legend>' +
+            '' +
+            '</body>';
+    });
+
+    it('should return Wednesday, 14th March', () => {
+        const rb = new RetrieveBin();
+        const result = rb.retrieveDataFromHtml(html);
+        result.should.be.equal('Wednesday, 14th March');
     });
 });
